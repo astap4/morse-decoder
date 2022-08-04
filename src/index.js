6b3af28e59c87050357a972650f864b6225b7e47
Alina <astapchik.1993@mail.ru>
2022-08-05 02:00:34 +0300

    feat: task morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,25 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let word = '';
+    let phrase = '';
+    let array = [];
+    for (let i = 0; i < (expr.length / 10); i++) {
+        array[i] = expr.slice(i * 10, ((i + 1) * 10))
+        if (array[i] === '**********')
+            phrase += ' '
+        if (array[i] != '**********') {
+            for (let j = 0; j < 10; j = j + 2) {
+                if (array[i].slice(j, j + 2) === '10')
+                    word += '.'
+                else if (array[i].slice(j, j + 2) === '11')
+                    word += '-'
+            }
+            phrase += MORSE_TABLE[word];
+            word = '';
+        }
+    }
+    return phrase
 }
 
 module.exports = {
